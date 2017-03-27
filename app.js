9369a4f40c5fad37fbcb5ffbd702d92e215dbe9b
Daniel Goldman <dzgoldman@wesleyan.edu>
2017-03-26 21:40:47 -0400

    gitignore, remove node modules

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const board = new five.Board();
 const morse = require('morse');
 const express  = require('express');
 const app      = express();
-const unit = 500
+const blink = require('./blink.js')
+var led;
 
-
+board.on("ready", () => {
+  led = new five.Led(11);
+})
 var bodyParser = require('body-parser');
 // app.use(express.bodyParser())
 app.use(bodyParser.json());
@@ -21,40 +24,11 @@ app.get('/', (req,res)=>{
 
 app.post('/',(req,res)=>{
   const text = req.body.text
-  board.on("ready", () => {
-    var led = new five.Led(11);
-    blink(text, 0, led)
-  })
-
-
+  blink(morse.encode(text), 0, led)
 })
+
+
+
 app.listen(3000, ()=> {
    console.log("listening on Port 3000")
 });
-
-const blink = (letters, currentIndex, led) => {
-    if (currentIndex >= letters.length) return
-    switch (letters[currentIndex]) {
-        case '.':
-            led.on()
-            setTimeout(() => {
-                led.off()
-                setTimeout(() => blink(letters, currentIndex + 1, led), unit)
-            }, unit)
-            break;
-        case '-':
-            led.on()
-            setTimeout(() => {
-                led.off()
-                setTimeout(() => blink(letters, currentIndex + 1, led), unit)
-            }, unit*3)
-            break;
-        case ' ':
-            setTimeout(() => {
-                blink(letters, currentIndex + 1, led)
-            }, unit*2)
-            break
-        default:
-          console.log('ERROR');
-    }
-}
---
+++ blink.js
@@ -0,0 +1,30 @@
+const unit = 500
+
+const blink = (letters, currentIndex, led) => {
+    if (currentIndex >= letters.length) return
+    switch (letters[currentIndex]) {
+        case '.':
+            led.on()
+            setTimeout(() => {
+                led.off()
+                setTimeout(() => blink(letters, currentIndex + 1, led), unit)
+            }, unit)
+            break;
+        case '-':
+            led.on()
+            setTimeout(() => {
+                led.off()
+                setTimeout(() => blink(letters, currentIndex + 1, led), unit)
+            }, unit*3)
+            break;
+        case ' ':
+            setTimeout(() => {
+                blink(letters, currentIndex + 1, led)
+            }, unit*2)
+            break
+        default:
+          console.log('ERROR');
+    }
+}
+
+module.exports = blink
